06faa842441bb9bd74dbfac2202c8419d2142dd3
mokhelek <mkatleho191@gmail.com>
2023-02-24 14:26:10 -0500

    added syntax highlighter to the blog

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -145,4 +145,11 @@ RECIPIENT_ADDRESS = "mkatleho191@gmail.com"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+
+
+
+
+
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
