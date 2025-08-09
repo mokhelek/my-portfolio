707fa9378afcc602b48b6f4159292d2b715e8c92
mokhelek <mkatleho191@gmail.com>
2025-08-09 16:00:19 +0200

    new design!!!

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,3 @@
-function myFunction(){
-  let more_description = document.getElementById("more-description");
-  let less_description = document.getElementById("less-description");
-
-  if (more_description.style.display === "none"){
-    more_description.style.display = "block";
-    less_description.style.display = "none";
-  }else{
-    more_description.style.display = "none";
-    less_description.style.display = "block";
-  }
-}
-
-
 function myFunction(id) {
   let more_description = document.getElementById("more-description" + id);
   let less_description = document.getElementById("less-description" + id);
